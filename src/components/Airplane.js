51289cc301227153af4bb3a4b474cbbfced6e02e
Soojin Hong <soojinhong@Soojins-MacBook-Pro.local>
2020-07-07 21:17:26 +1000

    component set up

---
+++ src/components/Airplane.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Airplane extends Component {
+  render() {
+    return(
+      <h1>Airplane coming soon</h1>
+    );
+  }
+}
+
+export default Airplane;
---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Airplane from './Airplane';
+
+function App() {
+  return (
+    <div className="App">
+      <Airplane />
+    </div>
+  );
+}
+
+export default App;
