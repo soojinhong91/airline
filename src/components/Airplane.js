cb32cdcb1c1db342c67b6e4df459a7d7cc6ea5b3
Soojin Hong <soojinhong@Soojins-MacBook-Pro.local>
2020-07-08 14:57:46 +1000

    working on connecting seed db

--- src/components/Airplane.js
+++ src/components/Airplane.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+const SERVER_URL1 = 'http://localhost:3000/airplines.json'
+const SERVER_URL2 = 'http://localhost:3000/flights.json'
 
 class Airplane extends Component {
   constructor(){
@@ -9,29 +11,93 @@ class Airplane extends Component {
       info: []
     };
 
-
-  const fetchInfo = () => {
-    axios.get(SERVER_URL).then((results) => {
-      this.setState({info: results.data});
-      setTimeout(fetchInfo, 5000);
-    });
-  };
-  fetchInfo();
+  this.fetchInfo();
 
   this.saveInfo = this.saveInfo.bind(this);
+  this.fetchInfo = this.fetchInfo.bind(this);
+}
+
+findflights(from, to) {
+  // loop
+  console.log(from, to)
+}
+
+fetchInfo () {
+  axios.get(SERVER_URL1).then((results) => {
+    this.setState({info: results.data});
+    setTimeout(this.fetchInfo, 5000);
+  });
+}
 
   saveInfo(from, to) {
-    axios.get(`${SERVER_URL}/${from}/${to}`).then((result) => {
+    axios.get(`${SERVER_URL1}/${from}/${to}`).then((result) => {
       this.setState({info: [...this.state.info, result.data]});
     });
   }
 
+fetchInfo2 () {
+  axios.get(SERVER_URL1).then((results) => {
+    this.setState({info: results.data});
+    setTimeout(this.fetchInfo, 5000);
+  });
+}
+
+  saveInfo2(from, to) {
+    axios.get(`${SERVER_URL1}/${from}/${to}`).then((result) => {
+      this.setState({info: [...this.state.info, result.data]});
+    });
+  }
 
   render() {
     return(
-      <h1>Airplane coming soon</h1>
+      <div>
+        <h1>Find Your Flight</h1>
+        <SearchForm onSubmit={this.findflights}/>
+        <Flights info={ this.state.info }/>
+      </div>
     );
   }
 }
+
+class SearchForm extends Component {
+  constructor(){
+    super();
+    this.state = {from: '', to: ''};
+    this._handleInputTo = this._handleInputTo.bind(this);
+    this._handleInputFrom = this._handleInputFrom.bind(this);
+    this._handleSubmit = this._handleSubmit.bind(this);
+  }
+  _handleInputFrom(event){
+    this.setState({from:event.target.value})
+  }
+  _handleInputTo(event) {
+    this.setState({to:event.target.value})
+  }
+  _handleSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.state.to, this.state.from);
+  }
+
+  render() {
+    return(
+      <div>
+      <p>Please search for a flight</p>
+        <form onSubmit={this._handleSubmit}>
+          From: <input type="text" placeholder="JFK" required onInput={this._handleInputFrom}/>
+          To: <input type="text" placeholder="SFO" required onInput={this._handleInputTo}/>
+          <input type="submit"/>
+        </form>
+      </div>
+    )
+  }
+}
+
+const Flights = (props) => {
+  return (
+    <div>
+      { props.info.map( (i) => <p>{i.id}:{i.from}:{i.to}</p>) }
+    </div>
+  );
+};
 
 export default Airplane;
